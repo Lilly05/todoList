ec4e7929ffeb9087cdd5b9ac04f0909eabb021bf
p20100 <71640994+Lilly05@users.noreply.github.com>
2022-06-08 20:14:01 +0200

    angular routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {CreateCardPage} from "./pages/create-card/create-card.page";
+import {CardsPage} from "./pages/cards/cards.page";
 
 const routes: Routes = [
   { path: '', redirectTo: 'cards', pathMatch: 'full'},
+  { path: 'cards', component: CardsPage},
   { path: 'create-card', component: CreateCardPage },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardsPage } from './pages/cards/cards.page';
